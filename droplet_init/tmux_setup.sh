b4db844a8dd375998a3ad457df466a657c279632
nikp359 <nik359@gmail.com>
2015-01-12 13:53:09 +0300

    tmux fix

--- droplet_init/tmux_setup.sh
+++ droplet_init/tmux_setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-TMUX_CONFIG="/home/nik/.tmux.conf"
+TMUX_CONFIG=~/.tmux.conf.1
+set -e
+cd ~
 echo "Tmux init"
 if [ -f "$TMUX_CONFIG" ]; then
 	echo "set -g terminal-overrides 'xterm*:smcup@:rmcup@'" >> $TMUX_CONFIG
 else
 	pwd ~
-	#touch $TMUX_CONFIG
+	touch $TMUX_CONFIG
 	echo "set -g terminal-overrides 'xterm*:smcup@:rmcup@'" >> $TMUX_CONFIG
 fi
