541e5b48df4984c0d819863c64151049ec69ff81
nikp359 <nik359@gmail.com>
2016-02-24 00:10:03 +0300

    Create server_thr.py

---
+++ server_thr.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+
+
+def conn_server(sock):
+    print "Start"
+    while True:
+        conn, addr = s.accept()
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            print data[:5]
+            if data[:5] == 'close':
+                break
+            else:
+                conn.send(data)
+        conn.close()
+
+
+def thread_start(thread_name):
+    thread_name = threading.Thread(target=conn_server, args=(s,))
+    thread_name.start()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 2222))
+s.listen(10)
+
+
+for i_thread_name in xrange(10):
+    thread_start("thread" + str(i_thread_name))
